e74e26306b97a3333ba63bb91ea4fc9f6b4b971e
Diego Perez Liebana <akilascartas@gmail.com>
2014-03-17 11:42:13 +0000

    changes for camera ready

--- code/src/framework/ExecSync.java
+++ code/src/framework/ExecSync.java
@@ -275,7 +275,7 @@ public class ExecSync extends Exec
      */
     public static void main(String[] args)
     {
-        m_mapNames = new String[]{"maps/ptsp_map01.map"}; //Set here the name of the map to play in.
+        m_mapNames = new String[]{"maps/ptsp_map40.map"}; //Set here the name of the map to play in.
         //m_mapNames = new String[]{"maps/ptsp_map01.map","maps/ptsp_map02.map","maps/ptsp_map08.map",
         //        "maps/ptsp_map19.map","maps/ptsp_map24.map","maps/ptsp_map35.map","maps/ptsp_map40.map",
         //        "maps/ptsp_map45.map","maps/ptsp_map56.map","maps/ptsp_map61.map"}; //In an array, to play in mutiple maps with runGames().
